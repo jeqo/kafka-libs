531bafd33e75f5399f47ee8bea5f42a95ad8370d
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-08-10 11:11:36 +0100

    feat: connect serde and schemas

---
+++ connect-schema-serde/src/test/java/kafka/serde/connect/example/AvroConverterExample.java
@@ -0,0 +1,30 @@
+package kafka.serde.connect.example;
+
+import io.confluent.connect.avro.AvroConverter;
+import io.confluent.connect.avro.AvroData;
+import java.util.Map;
+import kafka.serde.connect.SchemaAndValueSerde;
+
+public class AvroConverterExample {
+
+  public static void main(String[] args) {
+    var data = ksql.StockTrade.newBuilder()
+      .setAccount("123")
+      .setPrice(100)
+      .setQuantity(1)
+      .setSymbol("USD")
+      .setSide("A")
+      .setUserid("U001")
+      .build();
+    var avro = new AvroData(10);
+    var schemaAndValue = avro.toConnectData(data.getSchema(), data);
+    var converter = new AvroConverter();
+    converter.configure(Map.of(
+      "schema.registry.url", "http://localhost:8081"
+    ), false);
+    try (var serde = new SchemaAndValueSerde(converter)) {
+      var bytes = serde.serializer().serialize("test", schemaAndValue);
+      System.out.println(new String(bytes));
+    }
+  }
+}
---
+++ connect-schema-serde/src/test/java/kafka/serde/connect/example/JsonConverterExample.java
@@ -0,0 +1,26 @@
+package kafka.serde.connect.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import kafka.serde.connect.SchemaAndValueSerde;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.json.JsonConverter;
+
+public class JsonConverterExample {
+  public static void main(String[] args) {
+    var mapper = new ObjectMapper();
+    var jsonNode = mapper.createObjectNode().put("test", "t1").put("value", "v1");
+    var map = mapper.convertValue(jsonNode, new TypeReference<Map<String, Object>>() {
+    });
+
+    var converter = new JsonConverter();
+    converter.configure(Map.of(
+      "schemas.enabled", "false",
+      "converter.type", "value"
+    ));
+    var serde = new SchemaAndValueSerde(converter);
+    var bytes = serde.serializer().serialize("test", new SchemaAndValue(null, map));
+    System.out.println(new String(bytes));
+  }
+}
